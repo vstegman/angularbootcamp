a1d6e69198dd1b551a0958e1786c14fa04cd7d02
vstegman <vincesjunk@yahoo.com>
2015-03-25 15:55:38 -0500

    basic angular working

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp'),
+  connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: "www"
+  });
+});
+
+gulp.task('default', ['connect']);
---
+++ www/app/app.js
@@ -0,0 +1,4 @@
+angular.module('demoApp',[])
+  .controller("MainController", function($scope){
+    $scope.name = "Vince";
+  });
