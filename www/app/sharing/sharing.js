c80019e2fd6e9e9e8cf3ac40b3a2e4017b5634dc
vstegman <vincesjunk@yahoo.com>
2015-04-02 10:50:32 -0500

    sharing with a more complex service

--- www/app/sharing/sharing.js
+++ www/app/sharing/sharing.js
@@ -6,28 +6,41 @@ angular.module('sharing',['ui.router'])
   });
 })
 .service('widgetService', function(){
-  this.value = 0;
-})
-.factory('widgetFactory', function(){
-  return {
-    getWidget: function(name, start){
-      var value = start || 0;
-      return {name: name, value: value};
-    }
-  }
+  var self = this;
+  self.widgets = [];
+  self.value = function() {return self.widgets.length};
+  self.createWidget = function(name, start){
+      value = start || 0;
+      w = {name: name, value: value};
+      self.addWidget(w);
+      return w;
+  };
+  self.addWidget = function(widget){
+      self.widgets.push(widget);
+  };
 })
 .controller('firstCtrl', function(widgetService){
   var self = this;
+  self.mycount = 1;
+  self.newWidget = {name:'anonymous', value:1};
   self.widget = widgetService
-  self.addOne = function(){
-    self.widget.value += 1;
+  self.addWidget = function(){
+    widgetService.createWidget(self.newWidget.name, self.newWidget.value);
+    self.newWidget = {name:'anonymous', value:1};
+    self.mycount++;
   }
+  widgetService.createWidget('first is first', 12);
 })
 .controller('secondCtrl', function(widgetService){
   var self = this;
+  self.mycount = 1;
+  self.newWidget = {name:'unnamed', value:2};
   self.widget = widgetService
-  self.addTwo = function(){
-    self.widget.value += 2;
+  self.addWidget = function(){
+    widgetService.createWidget(self.newWidget.name, self.newWidget.value);
+    self.newWidget = {name:'unnamed', value:1};
+    self.mycount++;
   }
+  widgetService.createWidget('second is first', 24);
 })
 
