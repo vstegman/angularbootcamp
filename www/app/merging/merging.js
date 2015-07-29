2363c97f1a63f1e26f0fdfe5cce61bc3768c5980
vstegman <vincesjunk@yahoo.com>
2015-07-29 16:09:52 -0500

    more merging with resources

--- www/app/merging/merging.js
+++ www/app/merging/merging.js
@@ -1,47 +1,101 @@
 angular.module('merging',['ui.router'])
-.config(function($stateProvider){
+.config(function($stateProvider, $provide, $httpProvider){
   $stateProvider.state('merging', {
     url: '/merging',
     templateUrl: 'app/merging/merging.html',
     controller: 'MergingController',
     controllerAs: 'vm'
   });
+
+  $provide.factory('vsInterceptor', function($q){
+    return {
+//      'response': function(response){
+//        console.log('response intercepted');
+//        return response;
+//      },
+      'responseError': function(response){
+        if(response.status == 302){
+          console.log('redirect to login');
+        } else{
+          console.log('error intercepted');
+          return $q.reject(response);
+        }
+      }
+    }
+  });
+
+  $httpProvider.interceptors.push('vsInterceptor');
 })
-.controller('MergingController', function(api){
+.controller('MergingController', function(traitService){
   var self = this;
 
   self.ids = [1,2,3,4,5,6];
-  self.traits = [
-    {label: "Name",data:'name',selected: true},
-    {label: "Age",data:'age',selected: true},
-    {label: "Kids",data:'kids',selected: true},
-    {label: "Residence",data:'state',selected: true},
-    {label: "Married",data:'status', selected: true},
-  ];
+  self.traits = traitService.traits;
   self.results = [];
   self.selectedTraits = [];
 
-  self.call = function(data, id, container){
-    console.log("calling:  " + data);
-    api.get(data,id).then(function(response){
-      container[data] = response[data];
-      //self.results.push(response);
-      console.log("resolving:  " + data);
-    });
-  }
-
   self.addId = function(id){
-    self.selectedTraits = self.traits.filter(function(t){ return t.selected == true });
-    var display = {id: id};
-    self.results.push(display)
-    angular.forEach(self.selectedTraits, function(s){
-      display[s.data] = null;
-      self.call(s.data,id, display);
-    });
+    if(id != undefined) {
+      self.selectedTraits = traitService.selectedTraits(self.traits);
+      var display = {id: id};
+      self.results.push(display)
+      traitService.getTraits(self.selectedTraits, id, display)
+    }
   }
 
-  //self.call('age',3);
-  //self.call('status')
+
+})
+.service("traitService", function(api, traitConfig){
+  return {
+    traits: traitConfig.traits,
+
+    selectedTraits: function(list){
+        return list.filter(function(t){ return t.selected == true });
+    },
+
+    getTraits: function(selectedTraits, id, container){
+        var hitList = traitConfig.resourceList(selectedTraits);
+        angular.forEach(hitList, function(res){
+          api.get(res,id).then(function(response){
+            angular.forEach(traitConfig.resources[res], function(pt){
+              container[pt] = response[pt];
+            })
+          });
+        });
+      }
+  }
+})
+.service("traitCache", function(){
+  add = function(item){
+    cache[item.id] = item
+  };
+  cache = [];
+})
+.service("traitConfig", function(){
+  return {
+    traits: [
+              {label: "First",data:'name', resource: 'name', selected: true},
+              {label: "Last",data:'last',resource: 'name',selected: false},
+              {label: "Age",data:'age',resource: 'age',selected: true},
+              {label: "Kids",data:'kids',resource: 'kinder',selected: true},
+              {label: "Residence",data:'state',resource: 'state',selected: true},
+              {label: "Married",data:'status',resource: 'status', selected: true}
+            ],
+
+    resources:  {
+                  name: ['name','last'],
+                  age: ['age'],
+                  kinder: ['kids'],
+                  state: ['state'],
+                  status: ['status']
+                },
+
+    resourceList: function(traits) {
+      uniq = [];
+      angular.forEach(traits, function(t){ if(uniq.indexOf(t.resource) == -1){uniq.push(t.resource);} });
+      return uniq;
+    }
+  }
 
 })
 .service("api", function($http, $timeout){
@@ -50,22 +104,26 @@ angular.module('merging',['ui.router'])
       var url = "app/merging/data/" + type + ".json";
       return $http.get(url).catch(function(err){
         console.log(err);
+        return err;
       }).then(function(response){
-         return $timeout(function(){
-           if(id == undefined){
-            return response.data[type];
-            //return response.data;
-           } else {
-             return response.data[type].filter( function(e){
-               return e.id == id;
-             })[0];
-           };
-        }, Math.random()*3000);
+         if(response.status == 404){
+           data = {};
+           data[type] = 'err'
+           return data;
+         } else {
+           return $timeout(function(){
+             if(id == undefined){
+              return response.data[type];
+             } else {
+               return response.data[type].filter( function(e){
+                 return e.id == id;
+               })[0];
+             };
+           }, Math.random()*1000);
+         }
       });
-      //.error(function(data, status, headers){
-      //  console.log("error")
-      //})
     }
   }
 
 });
+
